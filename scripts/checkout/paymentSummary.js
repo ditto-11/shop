70206f8589df7cf650a2205274eeb80891f85b8e
Sir <duyhuu1109@gmail.com>
2024-08-30 23:28:47 +0700

    Make item() interactive

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import { cart } from "../../data/cart.js";
+import { cart, calculateCartQuantity } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOption.js";
 import { formatCurrency } from "../utils/money.js";
@@ -22,7 +22,7 @@ export function renderPaymentSummary() {
     <div class="payment-summary-title">Order Summary</div>
 
     <div class="payment-summary-row">
-    <div>Items (3):</div>
+    <div>Items (<span class="js-item-quantity">0</span>):</div>
     <div class="payment-summary-money">$${formatCurrency(
       productPriceCents
     )}</div>
@@ -57,4 +57,7 @@ export function renderPaymentSummary() {
     </button>
   `;
   document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
+  document.querySelector(
+    ".js-item-quantity"
+  ).innerHTML = `${calculateCartQuantity()}`;
 }
